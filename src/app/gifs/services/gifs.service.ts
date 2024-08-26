1e8cd967b85f307bacf5ebeb05f286544fa65ea3
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-25 19:55:54 -0500

    Validación de servicio para organizar los tags

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,8 +10,31 @@ export class GifsService {
     return [...this._history];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string): void {
+
+    // Pasar el tag a minúsculas
+    tag = tag.toLowerCase();
+
+    // Verifica si el tag ya existe en el historial
+    if ( this._history.includes(tag) ) {
+      // Si el tag ya existe, lo elimina del historial
+      this._history = this._history.filter((oldTag) => oldTag !== tag);
+    }
+
+    // Inserta nuevos tags al inicio del array y devuelve el array actualizado
     this._history.unshift(tag);
+
+    // Limita el historial a 10 elementos
+    this._history = this._history.splice(0, 10);
+  }
+
+
+  searchTag(tag: string): void {
+
+    // Valida que el tag no sea vacío
+    if ( tag.length === 0 ) return;
+    this.organizeHistory(tag);
+
     console.log(this._history);
 
   }
